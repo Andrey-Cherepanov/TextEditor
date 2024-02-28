0913290aeb761e75d657c87b0f2cb434df859f3a
Andrey Cherepanov <cherepanov.andrey944@gmail.com>
2024-02-28 16:34:04 +0300

    text layout

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import tkinter as tk
 def main():
     window = tk.Tk()
     window.title('Text Editor')
+    window.rowconfigure(0, minsize=400)
+    window.columnconfigure(1, minsize=500)
+
+    text_edit = tk.Text(window, font='Arial 14')
+    text_edit.grid(row=0, column=1)
 
     window.mainloop()
 
