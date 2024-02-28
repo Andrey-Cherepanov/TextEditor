f20bad2f73a6da56d098b34bc6a0dbd1f739d84f
Andrey Cherepanov <cherepanov.andrey944@gmail.com>
2024-02-28 16:39:59 +0300

    grid buttons

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ def main():
     text_edit = tk.Text(window, font='Arial 14')
     text_edit.grid(row=0, column=1)
 
+    frame = tk.Frame(window, relief=tk.RAISED, bd=2)
+    save_button = tk.Button(frame, text='Save')
+    open_button = tk.Button(frame, text='Open')
+
+    save_button.grid(row=0, column=0, padx=5, pady=5, sticky='ew')
+    open_button.grid(row=1, column=0, padx=5, sticky='ew')
+    frame.grid(row=0, column=0, sticky='ns')
+
     window.mainloop()
 
 if __name__ == '__main__':
